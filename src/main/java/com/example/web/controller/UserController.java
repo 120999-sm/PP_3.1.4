0b8334f1488b12c44caeb73d6c258c42e3fb5945
120999-sm <slavik.mixailov1999@gmail.com>
2023-12-06 15:04:29 +0300

    Initial commit

---
+++ src/main/java/com/example/web/controller/UserController.java
@@ -0,0 +1,36 @@
+package com.example.web.controller;
+
+import com.example.web.model.User;
+import com.example.web.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping()
+    public String userPage(Model model, Principal principal) {
+        User user = userService.findByUsername(principal.getName());
+        model.addAttribute("user", user);
+        return "user";
+    }
+}
+
+
+
+
+
+
---
+++ src/main/java/com/example/web/service/RoleService.java
@@ -0,0 +1,12 @@
+package com.example.web.service;
+
+import com.example.web.model.Role;
+
+import java.util.List;
+
+public interface RoleService {
+    List<Role> getAllRoles();
+    Role getRoleForName(String name);
+    Role getRoleById(Long id);
+    void addRole(Role role);
+}
